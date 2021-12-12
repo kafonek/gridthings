3c6e0ab4472cca8c4baf8fe1304dff57b8b2e2c7
Kafonek <matt.kafonek@noteable.io>
2021-12-12 10:45:02 -0500

    sorting out a strategy for __version__

--- src/gridthings/__init__.py
+++ src/gridthings/__init__.py
@@ -1,6 +1,10 @@
+from importlib_metadata import version
+
 from .cell import Cell, OutOfBoundsCell
 from .collection import Collection
 from .grid import Grid
 from .typed_grids import IntCell, IntGrid
 
-__version__ = "0.1.0"
+# Recommended way to handle __version__ when defining it only in pyproject.toml
+# https://github.com/python-poetry/poetry/issues/1036#issuecomment-489880822
+__version__ = version(__package__)
